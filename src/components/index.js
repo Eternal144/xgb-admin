f09e8a35dbffc00054f991180f96ae4c2a7e0bf0
Sisi Lee <1581618775@qq.com>
2019-07-08 18:22:51 +0800

    删除了动画

--- src/components/index.js
+++ src/components/index.js
@@ -22,8 +22,8 @@ import Drags from './ui/Draggable';
 import Edit from './edit/Edit';
 import Dashboard from './dashboard/Dashboard';
 import Gallery from './ui/Gallery';
-import BasicAnimations from './animation/BasicAnimations';
-import ExampleAnimations from './animation/ExampleAnimations';
+// import BasicAnimations from './animation/BasicAnimations';
+// import ExampleAnimations from './animation/ExampleAnimations';
 import AuthBasic from './auth/Basic';
 import RouterEnter from './auth/RouterEnter';
 import Cssmodule from './cssmodule';
@@ -38,7 +38,7 @@ const WysiwygBundle = Loadable({ // 按需加载富文本配置
 export default {
     BasicForm, BasicTable, AdvancedTable, AsynchronousTable,
     Echarts, Recharts, Icons, Buttons, Spins, Notifications,
-    Tabs, Banners, Drags, Dashboard, Gallery, BasicAnimations,
-    ExampleAnimations, AuthBasic, RouterEnter, WysiwygBundle,
+    Tabs, Banners, Drags, Dashboard, Gallery,
+     AuthBasic, RouterEnter, WysiwygBundle,
     Cssmodule, MapUi, QueryParams,Edit,NaviManage
 }
