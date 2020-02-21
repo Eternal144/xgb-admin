0741c2c1da270a0cdd1c0ee12682d765053aeddf
Sisi Lee <1581618775@qq.com>
2020-02-21 15:47:30 +0800

    今日最后一小改

--- src/components/navigation/NaviModify.jsx
+++ src/components/navigation/NaviModify.jsx
@@ -491,7 +491,7 @@ class NaviModify extends React.Component {
         if (parseInt(row.type) === 1) {
             let arr = ("" + row.link).split('=');
             let id = arr.length === 2 ? arr[1] : row.link
-            row.link = `/${data[0].rank}/${index}/column?columnId=${id}`
+            row.link = `/${data[0].id}/${index}/column?columnId=${id}`
         }
         newData[index] = row
         this.setState({ data: newData });
