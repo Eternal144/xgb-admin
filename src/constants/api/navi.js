ff1c287a86aa52ac79d219b0b904f710d39dc670
Sisi Lee <1581618775@qq.com>
2020-03-20 23:37:15 +0800

    删除前导航使用接口

--- src/constants/api/navi.js
+++ src/constants/api/navi.js
@@ -1,78 +1,14 @@
-
-
-export const getNaviInfo = () => {
-    return {
-        apiPath: `admin/menu`,
-        request: {
-            method: 'GET',
-            // mode: 'no-cors',
-
-        }
-    }
-}
-
-export const addNav = (body) => {
-    return {
-        apiPath: 'admin/addnav',
-        request: {
-            body: body,
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            // mode: 'no-cors',
-
-        }
-    }
-}
-
-
-
-export const getSecNaviList = (id) => {
-    return {
-        apiPath: `admin/secNav/${id}`,
-        request: {
-            method: 'GET',
-            // mode: 'no-cors'
-        }
-    }
-}
-
-//分页获取所有文章的第一页的内容。
-export const getmessageList = (id, page) => {
-    return {
-        apiPath: `admin/messageList/${id}/${page}`,
-        request: {
-            method: 'GET',
-            // mode: 'no-cors'
-        }
-    }
-}
 //获取二级导航下所有文章
 export const getNavAllArticle = (id) => {
     return {
         apiPath: `admin/allMessage/${id}`,
         request: {
             method: 'GET',
-            // mode: 'no-cors'
         }
     }
 }
 
-export const updateNav = (body) => {
-    return {
-        apiPath: `admin/updatenav`,
-        request: {
-            body: JSON.stringify(body),
-            method: 'POST',
-            // mode: 'no-cors',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        }
-    }
-}
-//获取该二级导航下所有文章的title和它的id。
+// //获取该二级导航下所有文章的title和它的id。
 export const getArticleTitle = (nav_id) => {
     return {
         apiPath: `admin/list/${nav_id}`,
@@ -83,8 +19,6 @@ export const getArticleTitle = (nav_id) => {
     }
 }
 
-
-//修改以后
 
 // 获取所有的导航信息
 export const getNaviLists = () => {
